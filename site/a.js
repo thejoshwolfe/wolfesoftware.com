b871a3e8f51d8b94d4da34c0b3aba4fd4181908a
Josh Wolfe <thejoshwolfe@gmail.com>
2015-09-22 08:48:46 -0700

    better tag line

--- site/a.js
+++ site/a.js
@@ -2,7 +2,7 @@ var sidebarContent = '' +
   '<div class="avatar" style="background-image:url(/site/josh.jpg)"></div>' +
   '<h3>Josh Wolfe</h3>' +
   '<div class="fancy-divider-line"></div>' +
-  '<p class="snippet">I like working on open-source games.</p>' +
+  '<p class="snippet">Software developer with a passion for compilers, games, and open-source software.</p>' +
   '<div class="fancy-divider-line"></div>' +
   '<ul class="nav">' +
   '  <li><a href="/">Home</a>' +
