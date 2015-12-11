a93183db9344a42624bdf7ded9ae6c6924bb3e6a
Josh Wolfe <thejoshwolfe@gmail.com>
2015-12-11 14:45:28 -0700

    sidebar work

--- site/sidebar.js
+++ site/sidebar.js
@@ -1,16 +1,10 @@
 document.getElementById("sidebar").innerHTML = '' +
-  '<h3>Josh Wolfe</h3>' +
+  '<h3><a href="/">Wolfe Software</a></h3>' +
   '<div class="fancy-divider-line"></div>' +
-  '<p class="snippet">Software developer with a passion for compilers, games, and open-source software.</p>' +
+  '<p class="snippet">Website for my open source projects</p>' +
   '<div class="fancy-divider-line"></div>' +
   '<ul class="nav">' +
-  '  <li><a href="/">Home</a>' +
-  '    <ul>' +
-  '      <li><a href="/#games">Games</a></li>' +
-  '      <li><a href="/#game-utilities">Utilities for Games</a></li>' +
-  '      <li><a href="/#academia">Academia</a></li>' +
-  '    </ul>' +
-  '  </li>' +
+  '  <li><a href="/">Home</a></li>' +
   '  <li><a href="/resume">Resume</a></li>' +
   '  <li><a href="http://github.com/thejoshwolfe">Github</a></li>' +
   '</ul>';
