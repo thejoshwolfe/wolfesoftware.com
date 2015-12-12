3df122283ea8fae75da15289eb0741bf7714e6a4
Josh Wolfe <thejoshwolfe@gmail.com>
2015-12-11 17:59:19 -0700

    game design study

--- site/sidebar.js
+++ site/sidebar.js
@@ -1,7 +1,7 @@
 document.getElementById("sidebar").innerHTML = '' +
   '<h3><a href="/">Wolfe Software</a></h3>' +
   '<div class="fancy-divider-line"></div>' +
-  '<p class="snippet">Website for my open source projects</p>' +
+  '<p class="snippet">Website for my open-source web projects</p>' +
   '<div class="fancy-divider-line"></div>' +
   '<ul class="nav">' +
   '  <li><a href="/">Home</a></li>' +
