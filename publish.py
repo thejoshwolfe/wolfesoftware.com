b619d6af984a9f459bb0c5cdf704b2b5f9e342bc
Josh Wolfe <thejoshwolfe@gmail.com>
2015-12-11 18:13:52 -0700

    publish script

---
+++ publish.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+
+if not input("did you remember to update Josh_Wolfe_resume.pdf? [yN] ").lower().startswith("y"):
+  print("interpreting answer as 'no'")
+  sys.exit(1)
+
+for item in [
+    "index.html",
+    "resume/",
+    "site/",
+  ]:
+  cmd = ["rsync", "-vuza", item, "server:public_http/" + item]
+  print(" ".join(cmd))
+  subprocess.check_call(cmd)
